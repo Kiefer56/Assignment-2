a7f0da832b7cca034752dd6d03150e51bda89899
cunninkf <cunninkf@IL-PC-139.uccc.uc.edu>
2018-01-23 12:16:35 -0500

    This is my second assignment

---
+++ Assignment02_cunninkf/src/cars/Cars.java
@@ -0,0 +1,26 @@
+/**
+ * Kiefer Cunningham
+ * cunninkf@mail.uc.edu
+ * 1/23/18
+ * Assignment2 
+ * Due/1/25/18
+ */
+package cars;
+
+/**this class will have the method and arguments of the 5 methods  
+ * @author cunninkf
+ *
+ */
+public class Cars {
+	int CarBrands;
+	int Ford;
+	int Chevy;
+	int Dodge;
+	int Chrystlerl;
+	int Honda;
+	int Mustang;
+	int Focus;
+	
+	
+	
+}
